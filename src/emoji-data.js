799c4e723a38d22d3073748b60dd8f9fb61a8624
Shaba9 <shaba92@gmail.com>
2018-11-26 14:35:44 -0800

    set up for vue. data done

---
+++ src/emoji-data.js
@@ -0,0 +1,29 @@
+const emojis = [
+  {
+    name: 'Sadorry',
+    image: 'https://goo.gl/images/GDc9Lk',
+    role: 'To make receiver feel guilty for not saying "yes"',
+    category: 'cute'
+  },
+
+  {
+    name: 'Thinky',
+    image: 'https://goo.gl/images/gyFgEg',
+    role: 'To make receiver question the decision',
+    category: 'intellectual'
+  },
+
+  {
+    name: 'Frusty',
+    image: 'https://goo.gl/images/XzNrC4',
+    role: 'Tell the receiver you are frustrated with the conversation',
+    category: 'informational'
+  },
+
+  {
+    name: 'Mischy',
+    image: 'https://goo.gl/images/Ly7nKj',
+    role: 'Inform that you are up to something',
+    category: 'informational'
+  }
+];
