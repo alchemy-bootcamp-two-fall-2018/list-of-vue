5ada38beb93bd043f5d9f386d46c61f80203fb4d
Kristin Hortsch <kristinhortsch@gmail.com>
2018-11-26 14:30:28 -0800

    set up for list vue lab and added in superhero-data.js

---
+++ src/superhero-data.js
@@ -0,0 +1,33 @@
+const superheroes = [
+    {
+        name: 'Batman',
+        location: 'Gotham',
+        image: 'https://vignette.wikia.nocookie.net/batman/images/8/8f/Christian_Bale_as_The_Dark_Knight.jpg/revision/latest?cb=20140208170841',
+        powers: 'exceptional martial artist, combat strategy, inexhuastible wealth, super cool gadgets/weapons'
+    },
+    {
+        name: 'Spiderman',
+        location: 'New York',
+        image: 'https://vignette.wikia.nocookie.net/marveldatabase/images/9/92/Peter_Parker_%28Earth-30847%29_from_Marvel_vs._Capcom_Infinite_0001.png/revision/latest?cb=20170922195944',
+        powers: 'superhuman strength & speed, web-slinging and wall-crawling abilities'
+    },
+    {
+        name: 'Wonder Woman',
+        location: 'Themyscira',
+        image: 'https://i.pinimg.com/originals/ab/5c/9f/ab5c9f3b968f8112b8139a8354a90977.jpg',
+        powers: 'super strength, flight, superhuman agility, combat skill, invulnerability'
+
+    },
+    {
+        name: 'The Flash',
+        location: 'Central City',
+        image: 'https://ips.pepitastore.com/storefront/img/resized/squareenix-store-v2/dffb07f41aa7989dfd25a493d5956302_1920_KR.jpg',
+        powers: 'super speed, intangibility, superhuman agility'
+    }
+
+
+
+
+];
+
+export default superheroes;
