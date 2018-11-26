abdf02b82fafef15fbc0321f63e79026f7298119
Marcy Silverman <marcysilve225@gmail.com>
2018-11-26 15:00:01 -0800

    set up files and created dog dataset

---
+++ src/dogs.js
@@ -0,0 +1,24 @@
+const dogs  = [
+    { 
+        name: 'French Bulldog',
+        image: 'https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/12194915/French-Bulldog-on-White-14.jpg', 
+        description: 'A pupper with bat ears and snores'
+    },
+    {
+        name:'NewFoundLander', 
+        image: 'https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/16191844/Newfoundland-onWhite-011.jpg', 
+        description: 'Large Woofer that likes to swim'
+    }, 
+    {
+        name: 'Australian Shepard', 
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLhPha-IPf_R9CVx4ybXtemHcuqTuNPRpcn3ZlFSzGwMfi-TaJ', 
+        description: 'A floofer that has butt wiggles when happy'
+    }, 
+    {
+        name: 'Poodle', 
+        image: 'http://poodleloverspost.com/wp-content/uploads/2013/12/bigstock-Standard-Poodle-50147513.jpg', 
+        description: 'Regal doggo with a floofy tail'
+    }
+]; 
+
+export default dogs; 
