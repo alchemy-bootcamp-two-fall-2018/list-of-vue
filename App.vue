6603ce14304b4bef47d9292bf32845a38f1347e5
Lise Brackbill <menagerie@hevanet.com>
2018-11-26 21:52:40 -0800

    adding image to test display

---
+++ App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="app">Female Ultrarunners
+    </div>
+</template>
+
+<script>
+import runners from '/runners.vue';
+
+ export default {
+    components: {
+        runners 
+    }
+};
+</script>
+
+<style>
+
+</style>
---
+++ runners.vue
@@ -0,0 +1,23 @@
+<template>
+    <ul>
+        <li>
+            <img src="/src/assets/catra.jpg">
+            <h3>Notable female ultrarunners</h3>
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+    li {
+        display:inline-block;
+        width 200 px;
+        height 200 px; 
+    }
+
+</style>
