2d0836cf27bdab002415e136885e6bcfe110cd74
Abel Quintero <abel.j.quintero@gmail.com>
2018-11-26 16:37:59 -0800

    updated main.js and setting up Bassists component

--- src/components/Bassists.vue
+++ src/components/Bassists.vue
@@ -1,18 +1,44 @@
 <template>
   <ul class="bassists">
       <li v-for="bassist in bassists"
-          v-bind:class="{ "
-
-      </li>
+          v-bind:class="{ bassist: true, selected: bassist === selected }"
+          v-bind:key="bassist.name"
+          v-on:click="onSelect(bassist)">
+          <img v-bind:src="bassist.image">
+          <h3>{{bassist.name}} shreds on a {{bassist.gear}}</h3>
+     </li>
+    </ul>
 </template>
 
 <script>
 export default {
-
-}
+    props: {
+        bassists: Array,
+        selected: Object,
+        onSelect: Function
+    }
+};
 </script>
 
 <style>
+    .bassists {
+        display: flex;
+        flex-wrap: wrap;
+    }
 
+    .bassist {
+        list-style: none;
+        position: relative;
+        width: 200px;
+        height: 200px;
+        border: 1px solid #aaa;
+        text-align: center;
+        margin-right: 3px;
+        cursor: pointer;
+    }
+
+    .bassist h3{
+        text-shadow: 1px 1px 1px white;
+    }
 </style>
 
