62259c6e0839820ce9397a2305d5655d165d49ee
JeiWolfei <jvampy22@gmail.com>
2018-11-27 08:53:54 -0800

    fixed errors

--- src/donuts.vue
+++ src/donuts.vue
@@ -1,12 +1,12 @@
 <template>
     <ul class= donuts>
-        <li v-bind:class="{ donut: true, selected: donut === selected }"    
-            v-for-key="donut in donuts"
-            v-bind:key"donut.name"
+        <!-- <li v-bind:class="{ donut: true, selected: donut === selected }"    
+            v-for="donut in donuts"
+            v-bind:"donut.name"
             v-on:click="onSelect(donut)">
             <h3>{{donut.name}} the {{donut.attribute}}</h3>
             <img v-bind:src="donut.image">
-        </li>
+        </li> -->
     </ul>
 </template>
                                  
