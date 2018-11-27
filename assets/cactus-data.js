67e25b5b7ed741165109a08a433594f820a3bec9
Christina T. Herlocker <cherlocker3113@gmail.com>
2018-11-26 21:09:18 -0800

    added list data and images

---
+++ assets/cactus-data.js
@@ -0,0 +1,18 @@
+const data = [
+    {
+        name: 'Prickly Pear',
+        huggability: '0/10',
+        image: './pear.jpg'
+    },
+    {
+        name: 'Moon Cactus',
+        huggability: '1/10',
+        image: './moon.jpg'
+    },
+    {
+        name: 'Fancy Blue Cactus',
+        huggability: '-4/10',
+        image: './blue.jpg'
+    }
+]
+export default data;
