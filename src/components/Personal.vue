586c4e94ffd84ccc04ecf5cf1bbe93e18148775d
Aaron Dennis <AMDennis1987@gmail.com>
2018-11-26 16:54:26 -0800

    added v-else functionality

--- src/components/Personal.vue
+++ src/components/Personal.vue
@@ -4,6 +4,7 @@
             <h2>You Chose {{guitarist.name}}</h2>
             <p>A {{guitarist.handed}} handed guitarist from {{guitarist.from}}</p>
         </div>
+        <div v-else><h3>Choose your favorite guitarist!</h3> </div>
     </section>
 </template>
 
