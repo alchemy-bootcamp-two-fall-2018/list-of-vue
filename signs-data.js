07ee009f65230fe72b9eff918e6036d461e5bceb
Heather <unphurlng@gmail.com>
2018-11-26 16:12:40 -0800

    added data.js file

---
+++ signs-data.js
@@ -0,0 +1,29 @@
+const signs = [
+    {
+        sign: 'Aries',
+        date: 'Mar 21 - Apr 19',
+        image: 'https://www.jessicaadams.com/wp-content/uploads/2018/02/Ari18profile.jpg'
+    },
+    {
+        sign: 'Taurus',
+        date: 'Apr 20 - May 20',
+        image: 'https://www.jessicaadams.com/wp-content/uploads/2018/02/Tau18profile.jpg?resolution=1280,1'
+    },
+    {
+        sign: 'Gemini',
+        date: 'May 21 - Jun 20',
+        image: 'https://www.jessicaadams.com/wp-content/uploads/2018/02/Gem18profile.jpg?resolution=1280,1'
+    },
+    {
+        sign: 'Cancer',
+        date: 'Jun 21 - Jul 22',
+        image: 'https://www.jessicaadams.com/wp-content/uploads/2018/02/Can18profile.jpg'
+    },
+    {
+        sign: 'Leo',
+        date: 'Jul 23 - Aug 22',
+        image: 'https://www.jessicaadams.com/wp-content/uploads/2018/02/Leo18profile.jpg'
+    }
+];
+
+export default signs;
