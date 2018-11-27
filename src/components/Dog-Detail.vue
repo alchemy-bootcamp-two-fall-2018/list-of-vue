e86a5662905a6bc704d082dcde55c930bd27f359
Ivan <i.poblete915@gmail.com>
2018-11-26 22:32:53 -0800

    set up dog-detail.vue

---
+++ src/components/Dog-Detail.vue
@@ -0,0 +1,23 @@
+<template>
+    <section>
+        <div v-if="dog" class="dog-detail">
+            <h2>{{dog.name}}</h2>
+            <img v-bind:src="dog.name">
+            <p><span>Breed: </span> {{dog.breed}}</p>
+            <p><span>Color: </span> {{dog.color}}</p>
+        </div>
+        <div class="message" v-else>Select a dog to view detail</div>
+    </section>
+</template>
+
+<script>
+export default {
+    props: {
+        dog: Object
+    }
+};
+</script>
+
+<style>
+
+</style>
