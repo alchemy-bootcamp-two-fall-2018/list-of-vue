0adf8fbfd3ba85f656926e0b55c4cd9ca45e2ad3
avanvalk <drewvv@gmail.com>
2018-11-26 20:09:02 -0800

    completed MVP

--- snowboards.js
+++ snowboards.js
@@ -2,19 +2,19 @@ const snowboards = [
     {
         name: 'burton',
         color: 'black',
-        image: '.src/assets/burton.jpg'
+        image: 'https://images.evo.com/imgp/700/133973/551343/burton-lip-stick-snowboard-women-s-2018-145.jpg'
     },
 
     {
         name: 'romeSDS',
         color: 'white',
-        image: '.src/assets/romeSDS.jpg'
+        image: 'https://cdn-us-ec.yottaa.net/53c6d66786305e30e100018d/acd43500c5780136ec32123dfe2baf36.yottaa.net/v~4b.20a/is/image/MoosejawMB/10355960_zm?$product700$&yocs=_&yoloc=us'
     },
 
     {
         name: 'neversummer',
         color: 'green',
-        image: './src/assets/neversummer.jpg'
+        image: 'https://images.evo.com/imgp/zoom/137074/568368/clone.jpg'
     },
 ];
 
