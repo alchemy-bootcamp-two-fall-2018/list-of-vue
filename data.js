604fbb1870687bbda0ad6b4c0084785563a0b0c5
Lise Brackbill <menagerie@hevanet.com>
2018-11-26 17:13:24 -0800

    basic file set up and database created

---
+++ data.js
@@ -0,0 +1,22 @@
+const runners = [
+    {
+        name: 'Catra Corbett',
+        image: '/src/assets/catra.jpg',
+        achievement: 'John Muir yo-yo FKT',
+    },
+    {
+        name: 'Rory Bosio',
+        image: '/src/assets/rory.jpg',
+        achievement: 'Two time winner, UTMB'
+    },
+    {
+        name: 'Krissy Moehl',
+        image: '/src/assets/krissy.jpg',
+        achievement: 'Hardrock winner and course record holder'
+    },
+    {
+        name: 'Amy Sproston'
+        image: '/src/assets/amy.jpg',
+        achievement: '4 time top 10 finisher at Western States'
+    }
+]
