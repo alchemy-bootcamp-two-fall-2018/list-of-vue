100f802c7c5c705f78bc331de3cb7f494d525239
PDXCrow <piratehans@gmail.com>
2018-11-26 17:09:03 -0800

    created initial Animal and Detail components

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <template>
   <div id="app">
-   
+    <img alt="Vue logo" src="./assets/logo.png">
+    <Animals msg="Hey it's animals"/>
+    <Detail msg="This is from Detail"/>
   </div>
 </template>
 
 <script>
-import pirates from './pirate-data';
+import Animals from './components/Animals.vue';
+import Detail from './components/Detail.vue';
 
 export default {
     name: 'app',
     components: {
-        HelloWorld
+        Animals,
+        Detail
     }
 };
 </script>
@@ -20,6 +24,8 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  text-align: center;
   color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
