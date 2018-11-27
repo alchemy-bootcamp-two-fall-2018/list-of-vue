c74ca9f645199273d445b3006ee47495fa57ed1e
Teonna Zaragoza <teonna.zaragoza@gmail.com>
2018-11-26 16:27:27 -0800

    added assets and race data

--- src/race-info.js
+++ src/race-info.js
@@ -1,15 +1,26 @@
-const aces = [
+const races = [
     {
         name: 'Mountain Lakes 100',
         image: '',
-        distance: '100 miles',
+        distance: ['100 miles'],
         qualifier: 'yes'
     },
-
     {
-        name: 'Siskyou Out and Back'
+        name: 'Siskyou Out and Back',
         image: '',
-        distance: ''
+        distance: ['100k', '50 miler', '50k', '15k'],
+        qualifier: 'no',
+    },
+    {
+        name: 'Zion Ultras & Half Marathon',
+        image: '',
+        distance: ['100 miler', '100k', '50k', 'half marathon'],
+        qualifier: 'yes'
+    },
+    {
+        name: 'Waldo 100k Trail Run',
+        image: '',
+        distance: ['100k'],
+        qualifier: 'yes'
     }
-
 ]
