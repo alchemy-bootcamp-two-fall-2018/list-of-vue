28699c0635348d6cdd3b1c2638ad54ddb6e7f0c5
Tyler Corbett <thetylercorbett@gmail.com>
2018-11-26 16:38:23 -0800

    can select dog and have name and rating display

---
+++ src/components/Player.vue
@@ -0,0 +1,21 @@
+<template>
+    <section>
+        <div v-if="dog">
+            <h2>{{dog.breed}}</h2>
+            <p>{{dog.breed}} has a rating of {{dog.rating}}</p>
+        </div>
+        <div v-else>Select your pup</div>
+    </section>
+</template>
+
+<script>
+export default {
+    props: {
+        dog: Object
+    }
+};
+</script>
+
+<style>
+
+</style>
