f3bf87b4d3d70f3861a29be7d16fb24cadf6b623
‘Michael <pickup.patterns@gmail.com>
2018-11-26 17:23:49 -0800

    added gif assets

---
+++ src/components/pokemon.vue
@@ -0,0 +1,23 @@
+<template>
+    <ul>
+        <li>
+            <img src="">
+            
+        </li>
+    </ul>        
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+li{
+display:flex;
+flexwrap:wrap;
+}
+
+
+</style>
