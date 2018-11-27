5debd6fa0b59e12d53d39591898d646bacf2d770
Abel Quintero <abel.j.quintero@gmail.com>
2018-11-26 16:10:26 -0800

    added images to assets and created data file

---
+++ bassist-data.js
@@ -0,0 +1,22 @@
+const bassists = [
+    {
+        name: 'Flea',
+        image: './src/assets/flea.jpg',
+        gear: 'Modulus Funk Unlimited'
+    },
+
+    {
+        name: 'Thundercat',
+        image: './src/assets/thundercat.jpg',
+        gear: 'Ibanez TCB1006 '
+    },
+
+    {
+        name: 'Esperanza Spalding',
+        image: './src/assets/Esperanza.jpg',
+        gear: 'Fender Jaco Pastorius Jazz Bass'
+    }
+
+];
+
+export default bassists;
---
+++ src/components/Bassists.vue
@@ -0,0 +1,18 @@
+<template>
+  <ul class="bassists">
+      <li v-for="bassist in bassists"
+          v-bind:class="{ "
+
+      </li>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
+
