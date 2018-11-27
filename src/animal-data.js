c869f9a4e4695fbefff9989e79670eb10c0fcad9
PDXCrow <piratehans@gmail.com>
2018-11-26 20:48:59 -0800

    added animal data

---
+++ src/animal-data.js
@@ -0,0 +1,22 @@
+const animals = [
+    {
+        name: 'Clucky',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/7/76/Braekel.jpg',
+        species: 'chicken',
+        status: 'hungry'
+    },
+    {
+        name: 'Bessy',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/c/cb/AyrshireCattle1.JPG',
+        species: 'cow',
+        status: 'satiated'
+    },
+    {
+        name: 'Porky',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/e/ed/Pig_suckling.png',
+        species: 'pig',
+        status: 'satiated'
+    }
+];
+
+export default animals;
