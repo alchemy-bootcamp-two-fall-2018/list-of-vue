6ffecf5c6498fde985220a8d5c793f703db22cd3
Tyler Corbett <thetylercorbett@gmail.com>
2018-11-26 16:22:08 -0800

    Dogs.vue setup

---
+++ src/components/Dogs.vue
@@ -0,0 +1,22 @@
+<template>
+    <ul class="dogs">
+        <li v-for="dog in dogs"
+            v-bind:class="{ dog: true, selected: dog === selected }"
+            v-bind:key="dog.breed"
+            v-on:click="onSelect(dog)">
+
+            <img v-bind:src="dog.image">
+            <h3>{{dog.breed}} {{dog.status}}</h3>
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/dog-data.js
@@ -0,0 +1,22 @@
+const dogs = [
+    {
+        breed: 'Golden Retriever',
+        image: 'https://vetstreet.brightspotcdn.com/dims4/default/b466b8e/2147483647/crop/0x0%2B0%2B0/resize/645x380/quality/90/?url=https%3A%2F%2Fvetstreet-brightspot.s3.amazonaws.com%2Ffa%2Fd15000a0d311e0a2380050568d634f%2Ffile%2FGolden-Retriever-3-645mk062111.jpg',
+        status: 'The Goodest Pup',
+        rating: '12/10'
+    },
+    {
+        breed: 'Dachshund',
+        image: 'https://cmeimg-a.akamaihd.net/640/cme/cuteness_data/s3fs-public/diy_blog/Personality-Traits-of-Dachshunds.jpg',
+        status: 'The Longest Pup',
+        rating: '11/10'
+    },
+    {
+        breed: 'Chihuahua',
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTf1kd0ZHTEj-I6S2-QIl5EDGCjP0oIbDUHRogNOMLbmwfnueG_',
+        status: 'The Smallest Pup',
+        rating: '9/10'
+    }
+];
+
+export default dogs;
