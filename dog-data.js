1c0dcb33cdf5c56d6cc47fd45cecfd5be67071b7
Ivan <i.poblete915@gmail.com>
2018-11-26 17:43:01 -0800

    created data file

---
+++ dog-data.js
@@ -0,0 +1,22 @@
+const dogs = [
+    {
+        name: 'Barry',
+        breed: 'Bernese Mountain',
+        color: 'Black/Brown',
+        image: './src/assets/bernese.jpg'
+    },
+    {
+        name: 'Sammie',
+        breed: 'Siberian Husky',
+        color: 'Grey/White',
+        image: './src/assets/siberian.jpg'
+    }, 
+    {
+        name: 'Charlie',
+        breed: 'Chihuahua',
+        color: 'Brown',
+        image: './src/assets/chihuahua.jpg'
+    }
+];
+
+export default dogs;
