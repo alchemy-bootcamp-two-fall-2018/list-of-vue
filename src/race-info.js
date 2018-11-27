5013c4664190343e7d277b90ad371b64fcd109fb
Teonna Zaragoza <teonna.zaragoza@gmail.com>
2018-11-26 16:13:28 -0800

    created files, added eslint

---
+++ src/race-info.js
@@ -0,0 +1,15 @@
+const aces = [
+    {
+        name: 'Mountain Lakes 100',
+        image: '',
+        distance: '100 miles',
+        qualifier: 'yes'
+    },
+
+    {
+        name: 'Siskyou Out and Back'
+        image: '',
+        distance: ''
+    }
+
+]
