9211da304067dc16553174f1301e7411b098da6d
Heather <unphurlng@gmail.com>
2018-11-30 06:07:52 -0800

    fixed property names, added date property

--- src/signs-data.js
+++ src/signs-data.js
@@ -1,27 +1,32 @@
 const signs = [
     {
-        name: 'Aries',
-        date: 'Mar 21 - Apr 19',
+        name: 'Moon in Aries',
+        date: 'Mar 21 to Apr 19',
+        description: 'With the Moon in Aries, the underlying evolutionary intention is to teach the heart courage. Thus, with the Moon in this sign, there is a reigning need for adventure. In order to feel comfortable and nourished, the individual requires challenge, newness, and some degree of tension. The ambient mood is one of urgency, edginess, and often of competitiveness.  Closeness and intimacy are frequently expressed through teasing. The domestic environment tends towards explosiveness and hair-on-fire pressure, yet there is a fierce sense of protectiveness toward it. Gone sour, the Aries Moon degenerates into an attitude of rage and resentment.',
         image: 'https://www.jessicaadams.com/wp-content/uploads/2018/02/Ari18profile.jpg'
     },
     {
-        name: 'Taurus',
-        date: 'Apr 20 - May 20',
+        name: 'Moon in Taurus',
+        date: 'Apr 20 to May 20',
+        description: 'With the Moon in Taurus, the underlying evolutionary intention is to teach the heart earthiness, peace, and acceptance of the animal nature. Thus, with the Moon in this sign, there is a reigning need for naturalness and ease. In order to feel comfortable and nourished, the individual requires the presence of Mother Nature, creature comforts, and unchallenged serenity. The ambient mood is one of earthy physicality.  Closeness and intimacy are frequently expressed through touch. The domestic environment tends towards predictability and steadiness, and there is a deep love toward it. Gone sour, the Taurus Moon degenerates into an attitude of stubborn rigidity.',
         image: 'https://www.jessicaadams.com/wp-content/uploads/2018/02/Tau18profile.jpg?resolution=1280,1'
     },
     {
-        name: 'Gemini',
-        date: 'May 21 - Jun 20',
+        name: 'Moon in Gemini',
+        date: 'May 21 to Jun 20',
+        description: 'With the Moon in Gemini, the underlying evolutionary intention is to teach the heart curiosity and communication skills. Thus, with the Moon in this sign, there is a reigning need for stimulus, conversation, and change. In order to feel comfortable and nourished, the individual requires variety and unpredictability. The ambient mood is one of scattered fascination.  Closeness and intimacy are expressed through words and the sharing of ideas and perceptions. The domestic environment tends towards chaos, change, and the intrusion of media. Gone sour, the Gemini Moon degenerates into shallowness and running around in circles.',
         image: 'https://www.jessicaadams.com/wp-content/uploads/2018/02/Gem18profile.jpg?resolution=1280,1'
     },
     {
-        name: 'Cancer',
-        date: 'Jun 21 - Jul 22',
+        name: 'Moon in Cancer',
+        date: 'Jun 21 to Jul 22',
+        description: 'With the Moon in Cancer, the underlying evolutionary intention is to heal the heart. Thus, with the Moon in this sign, there is a reigning need for loving home life and quiet time. In order to feel comfortable and nourished, the individual requires soulful, familiar intimacy. The ambient mood is one of guarded tenderness and vulnerability.  Closeness and intimacy are expressed through concern and nurture. The domestic environment tends towards the simple pleasures of the Hobbit-hole. Gone sour, the Cancer Moon degenerates into self-protective defensiveness, hypochondria, and endless fearfulness.',
         image: 'https://www.jessicaadams.com/wp-content/uploads/2018/02/Can18profile.jpg'
     },
     {
-        name: 'Leo',
-        date: 'Jul 23 - Aug 22',
+        name: 'Moon in Leo',
+        date: 'Jul 23 to Aug 22',
+        description: 'With the Moon in Leo, the underlying evolutionary intention is to teach the heart to feel safe enough to sing and roar. Thus, with the Moon in this sign, there is a reigning need for attention and opportunity for creative self-expression. In order to feel comfortable and nourished, the individual requires a sincerely appreciative audience, even if it is only one person. The ambient mood is one of dignified playfulness.  Closeness and intimacy are expressed through praise. The domestic environment tends towards grandness. Gone sour, the Leo Moon degenerates into pomposity, presumption and self-importance, driven by insecurity.',
         image: 'https://www.jessicaadams.com/wp-content/uploads/2018/02/Leo18profile.jpg'
     }
 ];
